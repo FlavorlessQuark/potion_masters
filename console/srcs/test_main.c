ee07aa4959e363ee3b576ed37db2d82df70944d9
FlavorlessQuark <flavorlessquark@gmail.com>
2023-09-20 18:37:03 -0700

    added test main

---
+++ console/srcs/test_main.c
@@ -0,0 +1,22 @@
+#include "../includes/splendor.h"
+
+int main()
+{
+	SDLX_RectContainer *root;
+	SDLX_Display *display;
+
+	SDLX_InitDefault();
+	root = loadConfig("assets/UI/boardUI");
+
+	// cleanupUIConfig(root);
+	display = SDLX_DisplayGet();
+
+	while (1)
+	{
+		SDLX_RenderReset(SDLX_DisplayGet());
+		SDLX_InputLoop();
+		print_config(SDLX_DisplayGet()->renderer ,root);
+		SDL_RenderPresent(SDLX_DisplayGet()->renderer);
+	}
+	SDLX_RenderQueuesCleanup();
+}
